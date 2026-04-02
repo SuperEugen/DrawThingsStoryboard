653bf2bfd5f12ba3fddf9b010b5606bdb64cdc2a
Ingo Hoffmann <ingo@ingohoffmann.de>
2026-04-02 22:52:16 +0200

    chore: remove obsolete files from v0.1

--- DrawThingsStoryboard/App/DetailRouter.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-/// DetailRouter is superseded by the three-pane NavigationSplitView in ContentView.
-/// Kept as a stub to avoid Xcode "missing reference" issues until old ImageGenerationView
-/// scaffolding is cleaned up in a follow-up.
-struct DetailRouter: View {
-    let selectedSection: AppSection?
-    var body: some View { EmptyView() }
-}
